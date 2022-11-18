b3735be2f6cb0d24369bd9e336c5aa8ef0551ff3
LinTongXue <2721183250@qq.com>
2022-11-19 00:18:37 +0800

    one

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+import BottomLayout from "./fixedLayout/bottomLayout.vue";
+</script>
+
+<template>
+  <BottomLayout/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/fixedLayout/bottomLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <van-tabbar v-model="active" @change="onChange">
+    <van-tabbar-item icon="home-o" name="home">首页</van-tabbar-item>
+    <van-tabbar-item icon="apps-o" name="type">类型</van-tabbar-item>
+    <van-tabbar-item icon="cart-o" name="shoppingTrolley">购物车</van-tabbar-item>
+    <van-tabbar-item icon="contact" name="mine">我的</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { Toast } from 'vant';
+const active = ref(0);
+const onChange = (index) => Toast(`标签 ${index}`);
+</script>
+
+<style scoped>
+
+</style>
